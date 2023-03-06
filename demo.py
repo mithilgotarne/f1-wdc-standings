d17ba1745c5e84e482da834112e1eb4c81b030ed
mithil <mithilgotarne@gmail.com>
2023-03-05 17:08:17 -0800

    added year flag

--- demo.py
+++ demo.py
@@ -1,15 +1,26 @@
 from moviepy.editor import *
 from drivers import *
-
 import os
+import argparse
 
-src_audio = AudioFileClip("media/bg.m4a")
+parser = argparse.ArgumentParser(description="Create WDC standings intro video")
+parser.add_argument("-y", "--year", type=int, help="The year for which you want to create the video")
+
+args = parser.parse_args()
+
+year = datetime.datetime.now().year
+
+if args.year:
+    year = args.year
+
+
+src_audio = AudioFileClip(f"media/{year}/bg.m4a")
 
 res = []
 
-for id in get_wdc_standings():
+for id in get_wdc_standings(year):
 
-    filename = 'media/videos/{}.mov'.format(id)
+    filename = f'media/{year}/videos/{id}.mov'
 
     if os.path.exists(filename):
 
@@ -20,9 +31,17 @@ if len(res) != 20:
 
 res.reverse()
 
-res = [VideoFileClip('media/videos/start.mov')] + res + [VideoFileClip('media/videos/end.mov')]
+res = [VideoFileClip(f'media/{year}/videos/start.mov')] + res + \
+    [VideoFileClip(f'media/{year}/videos/end.mov')]
 
-out: VideoClip = concatenate_videoclips(res)
-out = out.resize((1920, 1080)).set_audio(src_audio).set_fps(30)
+clip: VideoClip = concatenate_videoclips(res)
 
-out.write_videofile("out.mp4", codec='libx264', remove_temp=True)
+resolution = (1920, 1080)
+
+
+clip = clip.resize(resolution)
+
+clip = clip.set_audio(src_audio)
+clip = clip.set_fps(30)
+
+clip.write_videofile("out.mp4", codec='libx264', audio_codec="aac", remove_temp=True,)
--- drivers.py
+++ drivers.py
@@ -1,11 +1,12 @@
 import requests
+import datetime
 
 def get_default_list():
     return ['max_verstappen', 'perez', 'leclerc', 'sainz', 'russell', 'hamilton', 'norris', 'bottas', 'ocon', 'alonso', 'gasly', 'kevin_magnussen', 'vettel', 'ricciardo', 'tsunoda', 'zhou', 'mick_schumacher', 'albon', 'stroll', 'latifi']
 
 
-def get_wdc_standings():
-    response = requests.get('https://ergast.com/api/f1/current/driverStandings.json')
+def get_wdc_standings(year):
+    response = requests.get(f'https://ergast.com/api/f1/{year}/driverStandings.json')
 
     res = []
 
